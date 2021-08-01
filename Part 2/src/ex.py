250af79e7ac7ceb71e01e78b6bc15613fb8f143f
Dikea Sotiropoulou <p3160172@aueb.gr>
2021-08-01 15:29:28 +0300

    Files uploaded

---
+++ Part 2/src/ex.py
@@ -0,0 +1,40 @@
+def minus(x,y):
+	return x - y
+print k
+k=0
+
+tralala(56)
+
+def pro(x,y=2):
+    a = x + y
+    return a
+print pro(1)
+print pro(5,6)
+print pro(5,"kala") + 'str'
+
+x = 9
+print x+2
+
+l = 'sdfscd'++
+
+a= 5
+b= None
+print a + b
+
+
+
+def hello(x,y):
+   return "hello world"
+print hello(2,1)+2
+
+def add(x,y):
+    return x+y
+
+def add(x,y,z=1):
+    return x+y+z
+
+def mult(x,y,z):
+    return x*y*z
+
+def mult(x,y,z=1):
+    return x*y*z
---
+++ Part 2/src/example.py
@@ -0,0 +1,9 @@
+def add(x,y):
+   return x + y
+
+k="hello world"
+print add(2,k)
+
+k=None
+k=0
+print add(2,k)
